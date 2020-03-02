1dfc2b4fe06785861155bd78fcad20cc9110a186
Marks Polakovs <github@markspolakovs.me>
2020-03-02 00:00:50 +0000

    Swizzle FLIPS schema a bit

--- src/lib/FLIPS/index.ts
+++ src/lib/FLIPS/index.ts
@@ -2,52 +2,39 @@ import store from "../../store";
 
 export type FLIPSFeatureConfig =
 	| {
-			rule: "static";
-			enabled: boolean;
+			static: boolean;
 	  }
 	| {
-			rule: "env";
-			localhost: boolean;
-			dev: boolean;
-			staging: boolean;
-			prod: boolean;
+			env: {
+				localhost: boolean;
+				dev: boolean;
+				staging: boolean;
+				prod: boolean;
+			};
 	  }
-	| {
-			rule: "and";
-			and: FLIPSFeatureConfig[];
-	  }
-	| {
-			rule: "or";
-			or: FLIPSFeatureConfig[];
-	  }
-	| {
-			rule: "not";
-			not: FLIPSFeatureConfig;
-	  };
+	| { and: FLIPSFeatureConfig[] }
+	| { or: FLIPSFeatureConfig[] }
+	| { not: FLIPSFeatureConfig };
 
 export function evaluateFeature(feat: FLIPSFeatureConfig): boolean {
 	const state = store.getState();
-	switch (feat.rule) {
-		case "static":
-			return feat.enabled;
-		case "env":
-			let environment: "dev" | "staging" | "prod" | "localhost" =
-				state.GlobalConfig.myradio.environment;
-			if (
-				environment === "dev" &&
-				window.location.hostname === "localhost"
-			) {
-				environment = "localhost";
-			}
-			return feat[environment] === true;
-		case "and":
-			return feat.and.every(evaluateFeature);
-		case "or":
-			return feat.or.some(evaluateFeature);
-		case "not":
-			return !evaluateFeature(feat.not);
-		default:
-			throw new Error("Can't happen.");
+	if ("static" in feat) {
+		return feat.static;
+	} else if ("env" in feat) {
+		let environment: "dev" | "staging" | "prod" | "localhost" =
+			state.GlobalConfig.myradio.environment;
+		if (environment === "dev" && window.location.hostname === "localhost") {
+			environment = "localhost";
+		}
+		return feat.env[environment] === true;
+	} else if ("and" in feat) {
+		return feat.and.every(evaluateFeature);
+	} else if ("or" in feat) {
+		return feat.or.some(evaluateFeature);
+	} else if ("not" in feat) {
+		return !evaluateFeature(feat.not);
+	} else {
+		throw new Error("Unrecognised FLIPS rule " + JSON.stringify(feat));
 	}
 }
 
